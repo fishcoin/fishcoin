995201c4a59517cf2e2c5b8ecf3862947648dc56
Wim Van Renterghem <admin@Macintosh.local>
2014-01-25 00:32:14 +0100

    ready fo genesis

--- src/qt/bitcoinunits.cpp
+++ src/qt/bitcoinunits.cpp
@@ -34,9 +34,9 @@ QString BitcoinUnits::name(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("LTC");
-    case mBTC: return QString("mLTC");
-    case uBTC: return QString::fromUtf8("μLTC");
+    case BTC: return QString("FSC");
+    case mBTC: return QString("mFSC");
+    case uBTC: return QString::fromUtf8("μFSC");
     default: return QString("???");
     }
 }
@@ -45,9 +45,9 @@ QString BitcoinUnits::description(int unit)
 {
     switch(unit)
     {
-    case BTC: return QString("Litecoins");
-    case mBTC: return QString("Milli-Litecoins (1 / 1,000)");
-    case uBTC: return QString("Micro-Litecoins (1 / 1,000,000)");
+    case BTC: return QString("Fishcoins");
+    case mBTC: return QString("Milli-Fishcoins (1 / 1,000)");
+    case uBTC: return QString("Micro-Fishcoins (1 / 1,000,000)");
     default: return QString("???");
     }
 }
